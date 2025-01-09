37f2d1c7b4bb9beb59e6e0a4bfd0e637d040c061
tchristle <travis_christle@yahoo.com>
2025-01-08 21:59:13 -0600

    Update setup.ino
    Fixed axis orientations of all position array. Added position arrays for surface wrap animations.

--- neocube384/setup.ino
+++ neocube384/setup.ino
@@ -1,32 +1,35 @@
+// LED cube 8x8 setup functions
+// 01/08/2024
 
 void cube_array(void){
-
-  int i=0;
+  // assign x,y,z posistions to each pixel by panel
+  // float positions
+  int i=0, j=0;
   
-  //Serial.println(" = {");
-  //top
+  Serial.println("float array starting");
+  //top (1, +Z, 0:63)
   for(float j=-0.777; j<=0.777; j+=0.222){
     for(float k=-0.777; k<=0.777; k+=0.222){
         pixel_pos[i].x=k;
-        pixel_pos[i].y=j;
+        pixel_pos[i].y=-j;
         pixel_pos[i].z=1;
         pixel_pos[i].side=1;
         //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("1 },");
         i++;
     }
   }
-  //front
+  //back (2, -Y, 64:127)
   for(float j=-0.777; j<=0.777; j+=0.222){
     for(float k=-0.777; k<=0.777; k+=0.222){
-        pixel_pos[i].x=k;
+        pixel_pos[i].x=-j;
         pixel_pos[i].y=1;
-        pixel_pos[i].z=j;
+        pixel_pos[i].z=k;
         pixel_pos[i].side=2;
         //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.println("2 },");
         i++;
     }
   }    
-  //bottom
+  //bottom (5, -Z, 128:191)
   for(float j=-0.777; j<=0.777; j+=0.222){
     for(float k=-0.777; k<=0.777; k+=0.222){
         pixel_pos[i].x=k;
@@ -37,41 +40,248 @@ void cube_array(void){
         i++;        
     }
   }  
-  //back
+  //front (6, +Y, 192:255)
   for(float j=0.777; j>=-0.777; j-=0.222){
     for(float k=-0.777; k<=0.777; k+=0.222){
         pixel_pos[i].x=j;
         pixel_pos[i].y=-1;
-        pixel_pos[i].z=k;
+        pixel_pos[i].z=-k;
         pixel_pos[i].side=5;
         //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("5 },");
         i++;        
     }
   }
-  //left
+  //left (4, -X, 256:319)
   for(float j=-0.777; j<=0.777; j+=0.222){
     for(float k=-0.777; k<=0.777; k+=0.222){
         pixel_pos[i].x=-1;
-        pixel_pos[i].y=j;
-        pixel_pos[i].z=k;
+        pixel_pos[i].y=k;
+        pixel_pos[i].z=j;
         pixel_pos[i].side=3;
         //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("3 },");
         i++;        
     }
   }  
-  //right
+  //right (3, +X, 320:383)
   for(float j=-0.777; j<=0.777; j+=0.222){
     for(float k=-0.777; k<=0.777; k+=0.222){
         pixel_pos[i].x=1;
-        pixel_pos[i].y=j;
-        pixel_pos[i].z=k;
+        pixel_pos[i].y=k;
+        pixel_pos[i].z=-j;
         pixel_pos[i].side=4;
         //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("4 },");
         i++;        
     }
   }
-  //Serial.println(" };");
+  Serial.println("float array finished");
+
+  //..............................................................................................
+  // integer positions
+  i=0;
+  
+  Serial.println("integer array starting");
+  //top (1, +Z, 0:63)
+  for(float j=1; j<=8; j++){
+    for(float k=1; k<=8; k++){
+        pixel_pos[i].xi=k;
+        pixel_pos[i].yi=j; //was 9-j
+        pixel_pos[i].zi=9;
+        pixel_pos[i].side=1;
+        //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("1 },");
+        i++;
+    }
+  }
+  //back (2, -Y, 64:127)
+  for(float j=1; j<=8; j++){
+    for(float k=1; k<=8; k++){
+        pixel_pos[i].xi=9-j;
+        pixel_pos[i].yi=9;
+        pixel_pos[i].zi=k;
+        pixel_pos[i].side=2;
+        //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.println("2 },");
+        i++;
+    }
+  }    
+  //bottom (5 -Z, 128:191)
+  for(float j=1; j<=8; j++){
+    for(float k=1; k<=8; k++){
+        pixel_pos[i].xi=k;
+        pixel_pos[i].yi=j;
+        pixel_pos[i].zi=0;
+        pixel_pos[i].side=6;
+        //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("6 },");
+        i++;        
+    }
+  }  
+  //front (6, +Y, 192:255)
+  for(float j=1; j<=8; j++){
+    for(float k=1; k<=8; k++){
+        pixel_pos[i].xi=9-j; //was j
+        pixel_pos[i].yi=0;
+        pixel_pos[i].zi=k; //was 9-k
+        pixel_pos[i].side=5;
+        //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("5 },");
+        i++;        
+    }
+  }
+  //left (4, -X, 256:319)
+  for(float j=1; j<=8; j++){
+    for(float k=1; k<=8; k++){
+        pixel_pos[i].xi=0;
+        pixel_pos[i].yi=k;
+        pixel_pos[i].zi=j;
+        pixel_pos[i].side=3;
+        //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("3 },");
+        i++;        
+    }
+  }  
+  //right (3, +X, 320:383)
+  for(float j=1; j<=8; j++){
+    for(float k=1; k<=8; k++){
+        pixel_pos[i].xi=9;
+        pixel_pos[i].yi=9-k;
+        pixel_pos[i].zi=9-j;
+        pixel_pos[i].side=4;
+        //Serial.print("{"); Serial.print(j); Serial.print(", "); Serial.print(k); Serial.print(", "); Serial.print(1); Serial.print(", "); Serial.println("4 },");
+        i++;        
+    }
+  }
+  Serial.println("integer array finished");
+
+  //..............................................................................................
+  //define x,y,z axis wrap around arrays [8]x[32]
+  //top (1, +Z, 0:63)
+  //bottom (5 -Z, 128:191)
+  //left (4, -X, 256:319)
+  //right (3, +X, 320:383)
+  //front (6, +Y, 192:255)
+  //back (2, -Y, 64:127)
+  
+  // x_wrap, -Y,+Z,+Y,-Z, 0:7,8:15,16:23,24:31
+  //-Y
+  Serial.println("x_wrap indexes:");
+  for(i=64; i<=127; i++){
+    x_wrap[pixel_pos[i].zi-1][pixel_pos[i].xi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].zi-1); Serial.print(", "); Serial.print(pixel_pos[i].xi-1);Serial.println(", ");
+  }
+  //+Z
+  for(i=0; i<=63; i++){
+    x_wrap[pixel_pos[i].yi+8-1][pixel_pos[i].xi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].yi+8-1); Serial.print(", "); Serial.print(pixel_pos[i].xi-1);Serial.println(", ");
+  }  
+  //+Y
+  for(i=192; i<=255; i++){
+    x_wrap[pixel_pos[i].zi+16-1][pixel_pos[i].xi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].zi+16-1); Serial.print(", "); Serial.print(pixel_pos[i].xi-1);Serial.println(", ");
+  }
+  //-Z
+  for(i=128; i<=191; i++){
+    x_wrap[pixel_pos[i].yi+24-1][pixel_pos[i].xi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].yi+24-1); Serial.print(", "); Serial.print(pixel_pos[i].xi-1);Serial.println(", ");
+  }
+  
+  while(0){
+  //test cube wrap array
+  for(i=0; i<384; i++) { leds.setPixel(i,0);}
+  leds.show();
+  for(i=0; i<32; i++){
+    for(j=0; j<8; j++){    
+      leds.setPixel(x_wrap[i][j], RED);
+      leds.show();
+      Serial.println(x_wrap[i][j]);
+      //delay(10);  
+    }
+  }}
+  
+  //..............................................................................................
+  // y_wrap, -Z,+X,+Z,-X, 0:7,8:15,16:23,24:31
+  //top (1, +Z, 0:63)
+  //bottom (5 -Z, 128:191)
+  //left (4, -X, 256:319)
+  //right (3, +X, 320:383)
+  Serial.println("y_wrap indexes:");
+  //-Z
+  for(i=128; i<=191; i++){
+    y_wrap[pixel_pos[i].xi-1][pixel_pos[i].yi-1]=i;
+  }
+  //+X
+  for(i=320; i<=383; i++){
+    y_wrap[pixel_pos[i].zi+8-1][9-pixel_pos[i].yi-1]=i;
+  }  
+  //+Z
+  for(i=0; i<=63; i++){
+    y_wrap[9-pixel_pos[i].xi+16-1][9-pixel_pos[i].yi-1]=i;
+  }
+  //-X
+  for(i=256; i<=319; i++){
+    y_wrap[9-pixel_pos[i].zi+24-1][pixel_pos[i].yi-1]=i;
+  }
+
+  while(0){
+  //test cube wrap array
+  for(i=0; i<384; i++) {leds.setPixel(i,0);}
+  leds.show();
+  for(i=0; i<32; i++){
+    for(j=0; j<8; j++){    
+      leds.setPixel(y_wrap[i][j], GREEN);
+      leds.show();
+      Serial.println(y_wrap[i][j]);
+      //delay(10);  
+    }
+  }}
+  
+  //..............................................................................................
+  // z_wrap, -Y,+X,+Y,-X, 0:7,8:15,16:23,24:31
+  Serial.println("z_wrap indexes:");
+  //left (4, -X, 256:319)
+  //right (3, +X, 320:383)
+  //front (6, +Y, 192:255)
+  //back (2, -Y, 64:127)
+  //-Y
+  for(i=64; i<=127; i++){
+    z_wrap[pixel_pos[i].xi-1][pixel_pos[i].zi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].xi-1); Serial.print(", "); Serial.print(pixel_pos[i].zi-1);Serial.println(", ");
+  }
+  //+X
+  for(i=320; i<=383; i++){
+    z_wrap[pixel_pos[i].yi+8-1][pixel_pos[i].zi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].yi+8-1); Serial.print(", "); Serial.print(pixel_pos[i].zi-1);Serial.println(", ");
+  }  
+  //+Y
+  for(i=192; i<=255; i++){
+    z_wrap[9-pixel_pos[i].xi+16-1][9-pixel_pos[i].zi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].xi+16-1); Serial.print(", "); Serial.print(pixel_pos[i].zi-1);Serial.println(", ");
+  }
+  //-X
+  for(i=256; i<=319; i++){
+    z_wrap[pixel_pos[i].yi+24-1][pixel_pos[i].zi-1]=i;
+    Serial.print(i); Serial.print(", "); Serial.print(pixel_pos[i].yi+24-1); Serial.print(", "); Serial.print(pixel_pos[i].zi-1);Serial.println(", ");
+  }
+
+  while(0){
+  //test cube wrap array
+  for(i=0; i<384; i++) {leds.setPixel(i,0);} //clear cube
+  leds.show();
+  for(i=0; i<32; i++){
+    for(j=0; j<8; j++){    
+      leds.setPixel(z_wrap[i][j], BLUE);
+      leds.show();
+      Serial.println(z_wrap[i][j]);
+      //delay(10);  
+    }
+  }}
+
+  //define x,y,z axis wrap around arrays [8]x[32]
+  //top (1, +Z, 0:63)
+  //bottom (5 -Z, 128:191)
+  //left (4, -X, 256:319)
+  //right (3, +X, 320:383)
+  //front (6, +Y, 192:255)
+  //back (2, -Y, 64:127)
+
+ 
 }
+
 //////////////////////////////////////////////////////////////////////////////////////
 
 void color_rainbow(void){
